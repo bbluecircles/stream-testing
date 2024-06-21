d81574c8dddb3fc2c4a8aec094074f52728b1ee1
tobey14-tg <tobey14.tg@gmail.com>
2024-06-20 20:11:44 -0700

    Styling changes. Logic fixes.

--- src/webcam-view.js
+++ src/webcam-view.js
@@ -1,3 +1,13 @@
+const debounce = (callback, delay) => {
+    let timer;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            callback.apply(this, args)
+        }, delay);
+    }
+}
+
 class WebcamView {
     constructor() {
         this._isActive = false;
@@ -8,36 +18,50 @@ class WebcamView {
         this._recordedVideo = document.getElementById('recordedVideo');
         this._webcamViewElement = document.querySelector('.webcam-view');
 
-        this._recordedChunks = {};
+        this._recordedChunks = [];
 
         this._init();
     }
 
     _disableCamera = () => {
+        const disableCameraButton = document.querySelector('.js-disable-camera');
+
+        disableCameraButton.setAttribute('disabled', true);
         console.info('Disabling webcam...');
         this._webcamViewElement.classList.remove('webcam-active');
         this._mediaRecorder.stop();
     }
 
+    _setVideoElementSize = () => {
+        const webcamView = document.querySelector('.webcam-view');
+        const visibleVideo = document.getElementById('video');
+        const hiddenVideo = document.getElementById('recordedVideo');
+        const visibleWrapper = visibleVideo.parentElement;
+
+        if (!webcamView) return;
+
+        let { width, height } = webcamView.getBoundingClientRect();
+
+        width = width - (16 * 2);
+        height = height - (16 * 2);
+
+        visibleVideo.setAttribute('width', `${width}px`);
+        visibleVideo.setAttribute('height', `${height}px`);
+
+        hiddenVideo.setAttribute(`width`, `${width}px`);
+        hiddenVideo.setAttribute('height', `${height}px`);
+        
+    }
+
     _enableCamera = () => {
+        const disableCameraButton = document.querySelector('.js-disable-camera');
+
         console.info('Enabling webcam...');
+
+        disableCameraButton.removeAttribute('disabled');
 
         this._webcamViewElement.classList.add('webcam-active');
 
-
-        this._mediaRecorder.start();
-    }
-
-    _applyEventsToElements = () => {
-        const enableCameraButton = document.querySelector('.js-enable-camera');
-        const disableCameraButton = document.querySelector('.js-disable-camera');
-
-        enableCameraButton.addEventListener('click', this._enableCamera);
-        disableCameraButton.addEventListener('click', this._disableCamera);
-    }
-
-    _init = () => {
-        this._applyEventsToElements();
         navigator.mediaDevices.getUserMedia({ video: true })
         .then(stream => {
             try {
@@ -59,10 +83,28 @@ class WebcamView {
                     this._recordedVideo.src = url;
                 }
 
+                this._mediaRecorder.start();
+
             } catch(e) {
                 console.error(e)
             }
         })
+
+    }
+
+    _applyEventsToElements = () => {
+        const enableCameraButton = document.querySelector('.js-enable-camera');
+        const disableCameraButton = document.querySelector('.js-disable-camera');
+
+        enableCameraButton.addEventListener('click', this._enableCamera);
+        disableCameraButton.addEventListener('click', this._disableCamera);
+
+        window.addEventListener('resize', debounce(this._setVideoElementSize, 200))
+    }
+    
+    _init = () => {
+        this._applyEventsToElements();
+        this._setVideoElementSize();
     }
 }
 
